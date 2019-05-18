e0e6a173a9b86f553c34fad6d27b4dae11d1f101
dz1domin <36998838+dz1domin@users.noreply.github.com>
2019-05-18 21:29:58 +0200

    Add files via upload

--- PSiCAlgorithm.cpp
+++ PSiCAlgorithm.cpp
@@ -2,38 +2,32 @@
 
 std::string PSiCAlgorithm(std::string input)
 {
-	std::cout << input << std::endl;
+	std::cout << "PSiCAlgorithm input: " << input << std::endl;
 
 	// step 1
 	std::string last_24_digits_string(input.substr(2, input.size()));
 	cpp_int last_24_digits(removeLeadingZeros(last_24_digits_string));
-	std::cout << last_24_digits << std::endl;
 
 	// step 2
 	cpp_int multiplied = last_24_digits * 1000000;
-	std::cout << multiplied << std::endl;
 
 	// step 3
 	cpp_int increased = multiplied + 252100;
-	std::cout << increased << std::endl;
 
 	// step 4
-	cpp_int first_2_digits = increased;
-
-	while(first_2_digits > 100)
-		first_2_digits /= 10;
-
-	std::cout << first_2_digits << std::endl;
+	cpp_int first_2_digits = increased / cpp_int("10000000000000000000000000000");
 
 	// step 5
 	cpp_int result = last_24_digits + first_2_digits;
-	std::cout << result << std::endl;
 
 	// step 6
 	cpp_int modulo = result % 97;
-	std::cout << modulo << std::endl;
+	std::cout << "modulo result: " << modulo << std::endl;
 
-	return "";
+	if(modulo == 1)
+		return "PLACEHOLDER STRING TO BE CHANGED WHEN SETTINGS ARE IMPLEMENTED";
+	else
+		return input;
 }
 
 std::string& removeLeadingZeros(std::string& input)
