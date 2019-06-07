908e036ee71649763d3afe9405392167d9c63d1c
Dominik <dz1domin@gmail.com>
2019-06-07 21:54:57 +0200

    Algorithm documentation added

--- Server/include/PSiCAlgorithm/PSiCAlgorithm.hpp
+++ Server/include/PSiCAlgorithm/PSiCAlgorithm.hpp
@@ -1,3 +1,8 @@
+/**
+*	@file PSiCAlgorithm.hpp
+*	@brief This file contains the definition of PSiCAlgorithm that implements handling sequences specified in the task.
+*/
+
 #ifndef _PSiCAlgorithm_hpp_
 #define _PSiCAlgorithm_hpp_
 
@@ -7,10 +12,17 @@
 #include <vector>
 
 using cpp_int = boost::multiprecision::cpp_int;
-
+/**
+*	@class PSiCAlgorithm
+*/
 class PSiCAlgorithm
 {
 public:
+	/**
+	*	This member function implements looking for and replacing found sequences(in HTTP request body) if they fit the criteria.
+	*	@param input A reference to std::vector of chars to the HTTP request's body being parsed by the proxy.
+	*	@param desiredSequenceString A reference to string object containing the 26 number sequence the found one is to be swapped with.
+	*/
 	static void lookForSequences(std::vector<char>& input, const std::string& desiredSequenceString);
 
 private:
